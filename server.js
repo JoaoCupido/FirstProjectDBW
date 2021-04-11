4cbc3a7b56f6c496d5a4e3edf7209f578ea903b5
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2021-04-11 18:27:07 +0100

    + mongodb

--- server.js
+++ server.js
@@ -34,5 +34,29 @@ app.get('/signup/', function (req, res) {
     res.sendFile( __dirname + "/view/" + "signup.html" );
 })
 
+//user route with route parameters
+app.get('/user/:un/:pw/:av',function(req,res){
+    res.send("<h3>"+req.params.un+" "+req.params.pw+" "+req.params.av+"</h3>");
+
+});
+
+//user route with potential query string parameters
+app.get('/user',function (req, res){
+    //Use the 'url' dependency to parse the querystring
+    var queryObject = url.parse(req.url,true).query;
+
+    //if (ternary operator) the query Object is undefined or an empty object ("{}" without any key, value pairings) print a string, else stringify the query Object
+    res.send('User route' +
+        ((queryObject === undefined || Object.keys(queryObject).length === 0) ? ' without a query string.' : JSON.stringify(queryObject)));
+});
+
+app.post('/signup/', function(req,res){
+    console.log(req.body.username);
+    console.log(req.body.password);
+    console.log(req.body.avatar);
+
+    res.redirect('/user/'+req.body.username+'/'+req.body.password+'/'+req.body.avatar);
+});
+
 app.use(express.static(__dirname+'/view/'));
 // FONTE: https://stackoverflow.com/a/56505942

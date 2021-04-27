00d1442b5d5923ce7034addca42e6f86180f5a30
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2021-04-27 15:25:34 +0100

    corrigir erros de error loading images (css e html) , signup bem sucedido, falta: por fonte de profile picture, erro em fazer signin (login), foi feito o npm install socket.io, ajax e ejs (falta adicioná-los como constante no server.js)

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var app = express();
 
 app.use(urlencodedParser);
 
+app.use(express.static(__dirname+ "/view"));
+// FONTE: https://stackoverflow.com/a/56505942
+// https://stackoverflow.com/questions/48248832/stylesheet-not-loaded-because-of-mime-type
+
 mongoConfigs.connect(function(err){
     if(!err){
         app.listen(3000,function(){
@@ -30,6 +34,31 @@ app.get('/signin/', function (req, res) {
     res.sendFile( __dirname + "/view/" + "signin.html" );
 })
 
+app.post('/signin/', function (req, res) {
+    // ERROR: db not found
+    db.collection("G14").findOne({
+        "username": req.body.username
+    }, function(error, uname){
+        if (uname == null){
+            res.json({
+                "status": "error",
+                "message": "Account does not exist!"
+            });
+        }
+        else{
+            if(req.body.password === uname.password){
+                res.sendFile( __dirname + "/view/" + "home.html" );
+            }
+            else {
+                res.json({
+                    "status": "error",
+                    "message": "Incorrect password!"
+                });
+            }
+        }
+    })
+})
+
 app.get('/signup/', function (req, res) {
     res.sendFile( __dirname + "/view/" + "signup.html" );
 })
@@ -44,11 +73,8 @@ app.post('/signup/', function(req,res){
     NotesController.addNote(req, function () {
         //Let's return all activities once a new one is added
         NotesController.getAllNotes(function(result){
-            res.json(result); // alterar isto??
+            res.redirect( '/' );
         });
     });
 
 });
-
-app.use(express.static(__dirname+'/view/'));
-// FONTE: https://stackoverflow.com/a/56505942
